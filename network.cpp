30e95bd6e677fa229685e7fde4f29a76ba78626e
Henrik Heino <henrik.heino@gmail.com>
2022-05-22 22:07:38 +0300

    Linker bugfix: CTRL_ANY_MOVEMENT_KEY was not defined.

--- network.cpp
+++ network.cpp
@@ -7,10 +7,11 @@ const Urho3D::StringHash E_TO_CLIENT_SET_CONTROLLED_NODE("set_controlled_node");
 
 const Urho3D::StringHash P_ID("id");
 
-const unsigned CTRL_FORWARD = 1;
-const unsigned CTRL_BACKWARD = 2;
-const unsigned CTRL_LEFT = 4;
-const unsigned CTRL_RIGHT = 8;
-const unsigned CTRL_FIRE = 16;
+const unsigned CTRL_FORWARD = 0x01;
+const unsigned CTRL_BACKWARD = 0x02;
+const unsigned CTRL_LEFT = 0x04;
+const unsigned CTRL_RIGHT = 0x08;
+const unsigned CTRL_FIRE = 0x10;
+const unsigned CTRL_ANY_MOVEMENT_KEY = 0x0f;
 
 }
